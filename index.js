cd38184bb52b7f486a7ddf165ef91d087f8f969a
islammohmed <islammohamed3235@gmail.com>
2024-03-13 01:06:31 +0200

    finally

--- index.js
+++ index.js
@@ -6,12 +6,23 @@ import { dbConnection } from './db/db.connection.js'
 import { bootstrab } from './src/modules/index.routes.js'
 import { config } from 'dotenv'
 import { globalError } from './src/middleware/globalError.js'
+import { AppError } from './src/utils/AppError.js'
 config()
 cors()
 dbConnection()
 app.use(express.json())
 app.use('/uploads',express.static('uploads'))
 bootstrab(app)
-
+app.use('/',(req,res,next)=>{
+    res.send('welcome to our project')
+})
+app.use('*',(req,res,next)=>{
+    next(new AppError('Url Not Founded',404))
+})
 app.use(globalError)
 app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${port}!`))
+
+
+process.on('unhandledRejection', (err) => {
+    console.log('unhandledRejection', err);
+})
