32777f06248b27d00f68ad4d046f302380d285ab
islammohmed <islammohamed3235@gmail.com>
2024-04-13 20:35:07 +0200

    'accessuser'

--- src/modules/user/user.routes.js
+++ src/modules/user/user.routes.js
@@ -8,6 +8,7 @@ import {uploadsingleFile} from "../../../service/fileUpload/fileUpload.js"
 import { allowedTo } from "../../middleware/allowedTo.js";
 import {protectedRouter} from './../auth/controller/auth.js'
 import {checkEmail} from './../../middleware/checkEmail.js'
+import { isVerify } from "../../middleware/isVerify.js";
 userRouter
 .route('/') 
 .post(uploadsingleFile('image'),protectedRouter,validation(addUserValidation),allowedTo('admin'),checkEmail, addUser)
@@ -18,7 +19,7 @@ userRouter
 
 userRouter
 .route('/:id')
-.get(protectedRouter,validation(paramValidation),allowedTo('admin'),getSingleUser)
+.get(protectedRouter,isVerify,validation(paramValidation),allowedTo('admin','user'),getSingleUser)
 .delete(protectedRouter,validation(paramValidation),allowedTo('admin'),deleteUser)
 .put(uploadsingleFile('image'),protectedRouter,validation(updateUserValidation),allowedTo('admin','user','owner'),checkEmail, updateUser)
 export default userRouter
